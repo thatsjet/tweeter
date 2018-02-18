0e55c77080e958bce44cd7dcb06b3439dca10de6
thatsjet <janderson@miletwo.net>
2018-02-17 16:59:08 -0800

    added logging

--- tweet.py
+++ tweet.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # A simple tweet bot to grab my oldest tweet from a sqlite database,
 # Tweet it, update the date to today, and exit
 
@@ -8,19 +10,17 @@
 # Last Updated: 2018-02-11
 # Version 0.1a
 
-import sqlite3, tweepy, yaml, unittest
+import sqlite3, tweepy, yaml, logging
 from sqlite3 import Error
 
-# Setting to True will still get tweets but won't tweet it or update DB
-debug = False
+# Log output so we know this is running. I run this via cron, so
+# clean this up regularly if you do too.
+logging.basicConfig(filename='tweeter.log',level=logging.INFO)
+log = logging.getLogger(__name__)
 
-# define the main tweeting function
-def tweetit (tweet,file):
-    # make connection to the twitter API
-    print tweet,"\n"
-    success = False;
-    # close connection to twitter
-    return success;
+
+# Setting to True will still get tweets but won't tweet it or update DB
+debug = True 
 
 def create_connection(db_file):
     """ create a database connection to the SQLite database
@@ -32,6 +32,7 @@ def create_connection(db_file):
         conn = sqlite3.connect(db_file)
         return conn
     except Error as e:
+        log.error("failed to make database connection")
         print(e)
 
     return None
@@ -46,6 +47,7 @@ def get_oldest_tweet(conn):
     cur.execute('SELECT id, tweet, min(tdate) AS tdate FROM tweets')
     row = cur.fetchone()
     tweet = (row[0],row[1],row[2])
+    log.info("oldest tweet found: [id] " + str(row[0]))
     return tweet
 
 def tweet_that_shit(tweet):
@@ -53,7 +55,8 @@ def tweet_that_shit(tweet):
     auth = tweepy.OAuthHandler(keys["consumer_key"], keys["consumer_secret"])
     auth.set_access_token(keys["access_token"],keys["access_token_secret"])
     api = tweepy.API(auth)
-    if not debug: api.update_status(tweet+" | #FridayFunny #AppSec #DevSecOps")
+    api.update_status(tweet+" | #FridayFunny #AppSec #DevSecOps")
+    log.info("just tweeted: " + tweet)
     return
 
 def update_tweet_date(conn,id):
@@ -65,9 +68,9 @@ def update_tweet_date(conn,id):
     :return:
     """
     cur = conn.cursor()
-    if not debug: cur.execute("UPDATE tweets SET tdate=date('now','-1 day') WHERE id=?", [id])
-    if not debug: conn.commit()
-
+    cur.execute("UPDATE tweets SET tdate=date('now','-1 day') WHERE id=?", [id])
+    conn.commit()
+    log.info("Updated tweetID " + str(id) + " tdate to today")
     return
 
 def main():
@@ -79,9 +82,9 @@ def main():
         oldtweet = get_oldest_tweet(conn)
         tweetid = oldtweet[0]
         tweetit = oldtweet[1]
-        print tweetid,tweetit
-        #tweet_that_shit(tweetit)
-        update_tweet_date(conn,tweetid)
+        if debug: print tweetid,tweetit
+        if not debug: tweet_that_shit(tweetit)
+        if not debug: update_tweet_date(conn,tweetid)
 
 if __name__ == '__main__':
     main()
